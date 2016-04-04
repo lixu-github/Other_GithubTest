7179318873d54e3ed628c99fe9f5152ff9e72be1
dennis-li <lixu_chengdu@163.com>
2016-04-04 19:08:10 +0800

    update by lixu to lixu-github

---
+++ chapter_02/add_github_test.c
@@ -0,0 +1,7 @@
+
+iet g:netrw_dirhistmax  =10
+let g:netrw_dirhist_cnt =4
+let g:netrw_dirhist_1='/Users/lixu/Sites'
+let g:netrw_dirhist_2='/Users/lixu/C_Language/The_C_Programming_Language/chapter_06/_o'
+let g:netrw_dirhist_3='/Users/lixu/C_Language/Algorithms/C'
+let g:netrw_dirhist_4='/Users/lixu/C_Language/C++_Primer/.git'
